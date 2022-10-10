a31317cbc0ae535a68cb338331d5ab8fc7622db3
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-10-10 15:05:32 +0530

    add path planning with rtt

--- src/planning_utils.py
+++ src/planning_utils.py
@@ -1,8 +1,7 @@
-from enum import Enum
-from queue import PriorityQueue
 from bresenham import bresenham
 import numpy as np
 import constants as ct
+from sklearn.neighbors import KDTree
 
 
 def create_grid(data, drone_altitude, safety_distance):
@@ -43,122 +42,6 @@ def create_grid(data, drone_altitude, safety_distance):
     return grid, int(north_min), int(east_min)
 
 
-# Assume all actions cost the same.
-class Action(Enum):
-    """
-    An action is represented by a 3 element tuple.
-
-    The first 2 values are the delta of the action relative
-    to the current grid position. The third and final value
-    is the cost of performing the action.
-    """
-
-    WEST = (0, -1, 1)
-    EAST = (0, 1, 1)
-    NORTH = (-1, 0, 1)
-    SOUTH = (1, 0, 1)
-    # diagonal actions
-    NORTH_WEST = (-1, -1, 1.414)
-    NORTH_EAST = (-1, 1, 1.414)
-    SOUTH_WEST = (1, -1, 1.414)
-    SOUTH_EAST = (1, 1, 1.414)
-
-    @property
-    def cost(self):
-        return self.value[2]
-
-    @property
-    def delta(self):
-        return (self.value[0], self.value[1])
-
-
-def valid_actions(grid, current_node):
-    """
-    Returns a list of valid actions given a grid and current node.
-    """
-    valid_actions = list(Action)
-    n, m = grid.shape[0] - 1, grid.shape[1] - 1
-    x, y = current_node
-
-    # check if the node is off the grid or it's an obstacle
-
-    if x - 1 < 0 or grid[x - 1, y] == 1:
-        valid_actions.remove(Action.NORTH)
-    if x + 1 > n or grid[x + 1, y] == 1:
-        valid_actions.remove(Action.SOUTH)
-    if y - 1 < 0 or grid[x, y - 1] == 1:
-        valid_actions.remove(Action.WEST)
-    if y + 1 > m or grid[x, y + 1] == 1:
-        valid_actions.remove(Action.EAST)
-
-    # diagonal actions
-    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
-        valid_actions.remove(Action.NORTH_WEST)
-    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
-        valid_actions.remove(Action.NORTH_EAST)
-    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
-        valid_actions.remove(Action.SOUTH_WEST)
-    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
-        valid_actions.remove(Action.SOUTH_EAST)
-
-    return valid_actions
-
-
-def a_star(grid, h, start, goal):
-    path = []
-    path_cost = 0
-    queue = PriorityQueue()
-    queue.put((0, start))
-    visited = set(start)
-
-    branch = {}
-    found = False
-
-    while not queue.empty():
-        item = queue.get()
-        current_node = item[1]
-        if current_node == start:
-            current_cost = 0.0
-        else:
-            current_cost = branch[current_node][0]
-
-        if current_node == goal:
-            print('Found a path.')
-            found = True
-            break
-        else:
-            for action in valid_actions(grid, current_node):
-                # get the tuple representation
-                da = action.delta
-                next_node = (current_node[0] + da[0], current_node[1] + da[1])
-                branch_cost = current_cost + action.cost
-                queue_cost = branch_cost + h(next_node, goal)
-
-                if next_node not in visited:
-                    visited.add(next_node)
-                    branch[next_node] = (branch_cost, current_node, action)
-                    queue.put((queue_cost, next_node))
-
-    if found:
-        # retrace steps
-        n = goal
-        path_cost = branch[n][0]
-        path.append(goal)
-        while branch[n][1] != start:
-            path.append(branch[n][1])
-            n = branch[n][1]
-        path.append(branch[n][1])
-    else:
-        print('**********************')
-        print('Failed to find a path!')
-        print('**********************')
-    return path[::-1], path_cost
-
-
-def heuristic(position, goal_position):
-    return np.linalg.norm(np.array(position) - np.array(goal_position))
-
-
 def relative_grid_pose(local_pose, north_offset=0, east_offset=0):
     return int(local_pose[0]) - north_offset, int(local_pose[1]) - east_offset
 
@@ -197,3 +80,69 @@ def get_grid_goal(data, grid_start, goal_distance, north_offset, east_offset):
     else:
         return relative_grid_pose((np.random.choice(data[:, 0], size=1)[0], np.random.choice(data[:, 1], size=1)[0]),
                                   north_offset, east_offset)
+
+
+def get_random_point_in_free_space(free_space):
+    new_conf_index = np.random.randint(0, len(free_space[0]))
+    return free_space[0][new_conf_index], free_space[1][new_conf_index]
+
+
+def get_nearest_in_tree(nodes, node_to_query, return_distance=False):
+    kd_tree = KDTree(nodes, leaf_size=2)
+    # return index of the nearest node in nodes
+    nearest_node_distance, nearest_node_index = kd_tree.query([node_to_query], k=1, return_distance=True)
+
+    if not return_distance:
+        return nearest_node_index[0][0]
+    return nearest_node_distance, nearest_node_index[0][0]
+
+
+def get_new_node_on_grid(rand_conf, nearest_conf, delta):
+    x1, y1 = rand_conf[0], rand_conf[1]
+    x2, y2 = nearest_conf[0], nearest_conf[1]
+    if x1 == x2 and y1 == y2:
+        return False
+
+    total_d = pow((x1 - x2) ** 2 + (y1 - y2) ** 2, 0.5)
+    ratio_d = delta / total_d
+    new_conf_x, new_conf_y = ((1 - ratio_d) * x2) + ratio_d * x1, ((1 - ratio_d) * y2) + ratio_d * y1
+
+    return int(new_conf_x), int(new_conf_y)
+
+
+def is_edge_valid(grid, node1, node2):
+    bres_path = bresenham(int(node1[0]), int(node1[1]), int(node2[0]), int(node2[1]))
+
+    for point in bres_path:
+        if point[0] > 920 or point[1] > 920:
+            return False
+        if grid[point] != 0:
+            return False
+    return True
+
+
+def rtt(grid, start, goal, delta, max_points=10000):
+    free_space = np.where(grid == 0.0)
+    nodes = [start]
+    edges = []
+
+    while len(nodes) < max_points:
+        print(len(nodes))
+        rand_conf = get_random_point_in_free_space(free_space)
+        nearest_conf_index = get_nearest_in_tree(nodes, rand_conf)
+        new_conf = get_new_node_on_grid(rand_conf, nodes[nearest_conf_index], delta)
+        if not new_conf:
+            continue
+        if not is_edge_valid(grid, new_conf, nodes[nearest_conf_index]):
+            continue
+        nodes.append(new_conf)
+        edges.append((nearest_conf_index, len(nodes) - 1))
+
+        nearest_distance_from_goal, nearest_node_index_from_goal = get_nearest_in_tree(nodes, goal, True)
+        if nearest_distance_from_goal < delta:
+            nodes.append(goal)
+            edges.append((nearest_node_index_from_goal, len(nodes) - 1))
+            print(nearest_distance_from_goal, nodes[nearest_node_index_from_goal])
+            break
+
+    return nodes, edges
